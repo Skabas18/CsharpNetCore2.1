c3401dd648d2266357177d57a08a5b5026a62704
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-22 07:47:33 -0500

    Implementacion de la herencia, diferencia entre una clase abstracta y una clase sellada

--- Entidades/Alumno.cs
+++ Entidades/Alumno.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 
 namespace CoreEscuela.Entidades
 {
-    public class Alumno
-    {
-        public string UniqueId { get; private set; }
-        public string Nombre { get; set; }
-        public List<Evaluacion>Evaluaciones {get; set;} = new List<Evaluacion>();
-        public Alumno() => UniqueId = Guid.NewGuid().ToString();
+    public class Alumno: ObjetoEscuelaBase{
+    public List<Evaluacion>Evaluaciones {get; set;} = new List<Evaluacion>();
     }
 }
